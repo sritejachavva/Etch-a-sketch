f86b23e10f86346c9e72a1665d6e123ed1ac96f7
Sriteja Chavva <sritejachavva@gmail.com>
2023-10-09 23:03:21 -0700

    add rainbow button

--- myjs.js
+++ myjs.js
@@ -1,6 +1,24 @@
 
 
 /************* Create Divs *************/
+function selectMode(){
+    let rainbowButton = document.querySelector('.rainbow-mode > .temp_button');
+    let bool = true;
+    function toggleOnOff(){
+        bool = !bool;
+        if (bool == true){
+            rainbowButton.style.cssText = 'background-color:#efefef';
+        }
+        else {
+            rainbowButton.style.cssText = 'vertical-align: top; background: linear-gradient( 90deg, rgba(255, 0, 0, 1) 0%, rgba(255, 154, 0, 1) 10%, rgba(208, 222, 33, 1) 20%, rgba(79, 220, 74, 1) 30%, rgba(63, 218, 216, 1) 40%, rgba(47, 201, 226, 1) 50%, rgba(28, 127, 238, 1) 60%, rgba(95, 21, 242, 1) 70%, rgba(186, 12, 248, 1) 80%, rgba(251, 7, 217, 1) 90%, rgba(255, 0, 0, 1) 100% );';
+        }
+    }
+    rainbowButton.addEventListener('click',toggleOnOff);
+
+    rainbowButton.classList.add('button-clicked');
+    
+}
+
 
 function createDivs()
 {
@@ -42,3 +60,4 @@ function createDivs()
 }
 
 createDivs();
+selectMode();
