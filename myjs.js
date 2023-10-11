ccd72a2f3d42064f8298dda0488ccd7e5ae84c37
Sriteja Chavva <sritejachavva@gmail.com>
2023-10-10 23:17:02 -0700

    rgb change

--- myjs.js
+++ myjs.js
@@ -1,30 +1,34 @@
-
+let colorMode = 'normal';
+let bool = true;
 
 /************* Create Divs *************/
 function selectMode(){
     let rainbowButton = document.querySelector('.rainbow-mode > .temp_button');
-    let bool = true;
-    let colorMode = 'normal';
+    
     function toggleOnOff(){
         bool = !bool;
         if (bool == true){
             rainbowButton.style.cssText = 'background-color:#efefef';
             colorMode = 'normal';
-            createDivs(colorMode);
+            console.log('in true');
+            
         }
         else {
             colorMode = 'rainbow';
+            console.log("in false");
             rainbowButton.style.cssText = 'background: linear-gradient( 90deg, rgba(255, 0, 0, 1) 0%, rgba(255, 154, 0, 1) 10%, rgba(208, 222, 33, 1) 20%, rgba(79, 220, 74, 1) 30%, rgba(63, 218, 216, 1) 40%, rgba(47, 201, 226, 1) 50%, rgba(28, 127, 238, 1) 60%, rgba(95, 21, 242, 1) 70%, rgba(186, 12, 248, 1) 80%, rgba(251, 7, 217, 1) 90%, rgba(255, 0, 0, 1) 100% );';
-            createDivs(colorMode);
+            
         }
     }
     rainbowButton.addEventListener('click',toggleOnOff);
     rainbowButton.classList.add('button-clicked');
-    createDivs(colorMode);
+    return colorMode;
+    
 }
 
-function createDivs(colorMode)
+function createDivs()
 {
+    
     let emptyGrid = document.createElement('div');
     emptyGrid.classList.add('container');
     emptyGrid.style.cssText = 'border-style: solid; border-radius:10px; width:fit-content;'
@@ -37,10 +41,12 @@ function createDivs(colorMode)
     emptyCell.classList.add('cell');
     emptyCell.style.cssText = 'background-color:black; width:0.1px; height: 0.1px; border-style: solid; opacity:0.0;'
 
-
+    selectMode();
+    
     for (let i = 0; i < 64; i++) {
         let temprow = emptyRow.cloneNode(true);
         emptyGrid.appendChild(temprow);
+        
         for (let j = 0; j < 64; j++){
             let tempcell = emptyCell.cloneNode(true);
             
@@ -49,18 +55,18 @@ function createDivs(colorMode)
                 return cellOpacity + 0.3;
             }
             function fillCell(){
+                
                 newOpacity= increaseDarkness(tempcell);
                 console.log(`${colorMode}`);
                 if (colorMode == 'normal'){
-                    tempcell.style.backgroundColor = 'black';
-                    tempcell.style.cssText = `width:0.1px; height: 0.1px; border-style: solid; opacity:${newOpacity}`
+                    //tempcell.style.backgroundColor = 'black';
+                    tempcell.style.cssText = `width:0.1px; height: 0.1px; border-style: solid;background-color:black; opacity:${newOpacity}`
                 } else if (colorMode == 'rainbow'){
                     const randomR = Math.floor(Math.random() * 256);
                     const randomG = Math.floor(Math.random() * 256);
                     const randomB = Math.floor(Math.random() * 256);
-                    
-                    tempcell.style.cssText = `width:0.1px; height: 0.1px; border-style: solid; opacity:${newOpacity}`
-                    tempcell.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+                    tempcell.style.cssText = `width:0.1px; height: 0.1px; border-style: solid; opacity:${newOpacity}; background-color: rgb(${randomR}, ${randomG}, ${randomB})`
+                    //tempcell.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
 
                 }
                 // tempcell.style.cssText = `width:0.1px; height: 0.1px; border-style: solid; background-color:black; opacity:${newOpacity}`
@@ -74,5 +80,16 @@ function createDivs(colorMode)
        
 }
 
-selectMode();
+createDivs();
+
+
+
+/*
+1) Generate Grid
+2) add event listener to rainbow button 
+3) 
+3) Color grid based on hover event based on model
+4) Update color?
+
+*/
 
